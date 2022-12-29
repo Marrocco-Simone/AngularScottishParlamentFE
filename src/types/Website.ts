298a56226e4e17810be129c7fb5c6123b46a4fce
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-12-29 22:38:43 +0100

    added website type

---
+++ src/types/Website.ts
@@ -0,0 +1,7 @@
+export type Website = {
+  ID: number;
+  PersonID: number;
+  WebSiteTypeID: number;
+  WebURL: string;
+  IsDefault: boolean;
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export { Member } from './Member';
 export { Party } from './Party';
 export { MemberParties } from './MemberParties';
+export { Website } from './Website';
