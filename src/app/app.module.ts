937b27ffc992934c33a49d79d682c998bb40502a
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-12-29 19:16:16 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { MinimizedPersonComponent } from './minimized-person/minimized-person.component';
 import { FullPersonComponent } from './full-person/full-person.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     MinimizedPersonComponent,
-    FullPersonComponent
+    FullPersonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
