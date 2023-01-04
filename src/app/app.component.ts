bf273a0effd87886394fe7bbdf2977eb03d17a5e
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-01-04 16:05:15 +0100

    services loaded on startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MembersService } from './members.service';
+import { PartiesService } from './parties.service';
+import { WebsitesService } from './websites.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    public membersService: MembersService,
+    public partiesService: PartiesService,
+    public websiteService: WebsitesService
+  ) {}
+}
